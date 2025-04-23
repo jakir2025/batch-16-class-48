ad2dded99a2ed9feed6ff3cb5015357256db0c12
jakir2025 <jakirhusen5002@gmail.com>
2025-04-23 16:40:52 +0600

    crud operation 01

---
+++ config.php
@@ -0,0 +1,15 @@
+<!-- config -->
+
+<?php
+//hostname,usename,password,databaseName
+
+ $connection =  New mysqli ("localhost" , "root", "" ,"students" );
+
+ //check database connection quary
+//  if($connection == true){
+//     echo "connected successfully!";
+//  }
+//  else{
+//     echo "connented failed";
+//  }
+?>
